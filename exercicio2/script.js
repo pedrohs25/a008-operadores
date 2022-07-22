123c44b521273111d8fdb455935e915c7913b87a
pedrohs25 <pedroh.cec.silva@gmail.com>
2022-07-21 21:11:15 -0300

    Feito os exercícios a008-operadores

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let pergNumero1 = Number (prompt ("Insira um número"))
+let pergNumero2 = Number (prompt ("Insira um número"))
+
+
+console.log("O primeiro numero é maior que segundo?", pergNumero1 > pergNumero2);
+console.log("O primeiro numero é igual ao segundo?", pergNumero1 === pergNumero2);
+console.log("O primeiro numero é divisível pelo segundo?", pergNumero1 % pergNumero2 ===0);
+console.log("O segundo numero é divisível pelo primeiro?", pergNumero2 % pergNumero1 ===0);
---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const a = 5 > 20 && 5 < 2
+console.log(a);
+
+const b = 5 === 5 || 5 === "5"
+console.log(b);
+
+const c = !20>50
+console.log(c);
+
+const d = !(20 > 50)
+console.log (d)
